eeeddea0548c529b36d434c90c069e111ec4089a
Egor Lynko <flexoid@gmail.com>
2013-05-26 23:14:41 +0300

    Add summary

--- PhoneCatalog/Scripts/app/factories/phone.js
+++ PhoneCatalog/Scripts/app/factories/phone.js
@@ -1,3 +1,9 @@
 CatalogApp.factory('Phone', function($resource) {
-  return $resource('/api/phones/:id', { id: '@id' }, { update: { method: 'PUT' } } );
+  var Phone = $resource('/api/phones/:id', { id: '@id' }, { update: { method: 'PUT' } } );
+  Phone.prototype.summary = function() {
+    return this.OS + ', экран\u00A0' + this.ScreenSize + '\u00A0(' + this.Resolution +
+      '), процессор\u00A0' + this.Processor + ', камера\u00A0' + this.CameraPixelCount +
+      ', аккумулятор\u00A0' + this.BatteryCapacity;
+  };
+  return Phone;
 });
